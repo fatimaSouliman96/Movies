0bab048a09f2803d5b2c63d71d12e2379dd78d89
fatimasouliman <fatima96souliman@gmail.com>
2025-01-25 20:51:55 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // Replace 'repository-name' with your repository name
 export default defineConfig({
-  base: 'Movies',
+  base: '/Movies/',
   plugins: [react()],
 });
